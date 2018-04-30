547b87bc95d1f227d90d857ef8589b75e1fc5c4e
'amine <'el.amine.aoula@gmail.com'>
2018-04-30 14:21:35 +0300

    Amine: Adding RobotFrameWork project

---
+++ RikuRobot/src/main/java/fi/otso/henki/riku/robot/RikuRobot/App.java
@@ -0,0 +1,13 @@
+package fi.otso.henki.riku.robot.RikuRobot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
